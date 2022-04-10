c1a720af4d7fe871da00fde277010c79adc2f081
TonmoyGoon <tonmoygoon@gmail.com>
2022-04-10 17:14:55 +0600

    commiting

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const allDept = ["ye", "fe", "wpe", "ae", "tem", "tfd", "ipe", "tmdm", "dce", "e
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = "mongodb://localhost:27017/studentInfo";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/studentInfo";
 // "mongodb://localhost:27017/studentInfo"
 
 mongoose.connect(dbUrl)
